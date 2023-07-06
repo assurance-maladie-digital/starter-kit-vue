98f3c265a24b9fcfef607b418ce0544e9fee6235
David FYON <davidfyon@gmail.com>
2023-07-06 10:15:30 +0200

    add DayJS nuxt plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,10 @@ import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@invictus.codes/nuxt-vuetify'],
+  modules: [
+    '@invictus.codes/nuxt-vuetify',
+    'dayjs-nuxt',
+  ],
   vuetify: {
     /* vuetify options */
     vuetifyOptions: {
@@ -25,5 +28,11 @@ export default defineNuxtConfig({
       autoImport: true,
       importLabComponents: true,
     },
+  },
+  dayjs: {
+    locales: ['en', 'fr'],
+    plugins: ['relativeTime', 'utc', 'timezone'],
+    defaultLocale: 'fr',
+    defaultTimezone: 'Europe/Paris',
   }
 });
