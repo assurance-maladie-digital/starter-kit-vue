cc2f9e87ad79a7826311ec77200f6661db478764
David FYON <davidfyon@gmail.com>
2023-07-07 14:57:00 +0200

    fix indentations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,29 +1,29 @@
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@invictus.codes/nuxt-vuetify'],
-  vuetify: {
-    /* vuetify options */
-    vuetifyOptions: {
-      icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-          mdi,
-        },
-      },
-    },
+	devtools: { enabled: true },
+	modules: ["@invictus.codes/nuxt-vuetify"],
+	vuetify: {
+		/* vuetify options */
+		vuetifyOptions: {
+			icons: {
+				defaultSet: "mdi",
+				aliases,
+				sets: {
+					mdi,
+				},
+			},
+		},
 
-    moduleOptions: {
-      /* nuxt-vuetify module options */
-      treeshaking: true,
-      useIconCDN: true,
+		moduleOptions: {
+			/* nuxt-vuetify module options */
+			treeshaking: true,
+			useIconCDN: true,
 
-      /* vite-plugin-vuetify options */
-      styles: true,
-      autoImport: true,
-      importLabComponents: true,
-    },
-  }
+			/* vite-plugin-vuetify options */
+			styles: true,
+			autoImport: true,
+			importLabComponents: true,
+		},
+	},
 });
