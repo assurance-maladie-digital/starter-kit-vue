7687d4ccb08756545ead97426e5954454a656088
David FYON <davidfyon@gmail.com>
2023-07-10 14:57:12 +0200

    add snapshots

--- components/MyTest/tests/MyTest.spec.ts
+++ components/MyTest/tests/MyTest.spec.ts
@@ -6,6 +6,6 @@ import MyTest from '../MyTest.vue';
 describe('MyTest', async () => {
 	it('is a Vue instance', () => {
 		const wrapper = mount(MyTest);
-		expect(wrapper.vm).toBeTruthy();
+		expect(wrapper).toMatchSnapshot();
 	});
 });
