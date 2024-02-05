d00914a491288b03732da141f65c90d7f3ec46fe
virginieVachet <vachetvirginie@gmail.com>
2024-02-05 10:37:41 +0100

    test ok

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
 		<hr class="mt-16">
+		<MyButton />
 		<v-icon icon="mdi-home-outline" />
 		<v-btn variant="tonal" color="primary" class="ml-5"> Button </v-btn>
 		<ContentDoc />
 		<TestComponent />
 	</div>
 </template>
+<script setup>
+import { MyButton } from "vivvi-lib";
+</script>
 <style lang='scss'>
 </style>
