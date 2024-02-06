93eefca55ea980c7c64939ce21fa735344045a57
David FYON <davidfyon@gmail.com>
2024-02-06 15:52:34 +0100

    update version + redo package import

--- pages/index.vue
+++ pages/index.vue
@@ -5,20 +5,5 @@
 		<v-btn variant="tonal" color="primary" class="ml-5"> Button </v-btn>
 		<ContentDoc />
 		<TestComponent />
-
-		<PageContainer>
-			<VCard class="pa-8">
-				Contenu de la page.
-			</VCard>
-		</PageContainer>
-		<PageContainer size="m">
-			<VCard class="pa-8">
-				Contenu de la page.
-			</VCard>
-		</PageContainer>
 	</div>
 </template>
-
-<script setup lang="ts">
-import {PageContainer} from 'bridge-david';
-</script>
