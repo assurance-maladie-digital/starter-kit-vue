9524e30310c37717c733a3573208691e56e21615
David FYON <davidfyon@gmail.com>
2024-02-26 17:15:12 +0100

    fix component imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,9 @@
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
-import Components from 'unplugin-vue-components/vite'
+import Components from "unplugin-vue-components/vite";
 
 export default defineNuxtConfig({
+	ssr: false,
 	devtools: { enabled: true },
-	build: {
-		transpile: ["vuetify"],
-	},
 	modules: [
 		(_options, nuxt) => {
 			nuxt.hooks.hook("vite:extendConfig", (config) => {
@@ -18,6 +16,9 @@ export default defineNuxtConfig({
 		"@pinia/nuxt",
 		"@nuxt/test-utils/module"
 	],
+	build: {
+		transpile: ["vuetify"],
+	},
 	vite: {
 		vue: {
 			template: {
@@ -25,25 +26,24 @@ export default defineNuxtConfig({
 			},
 		},
 		// change the component dirs to match our project layout
-		plugins: [Components({ dts: true, dirs: ["./components"]})],
+		plugins: [
+			vuetify(),
+			Components({ dts: true, dirs: ["./components"]})
+		],
 	},
-
 	css: [
 		"bridge-david/style.css",
 		"~/assets/styles/index.scss"
 	],
-
 	imports: {
 		dirs: ["./stores"],
 	},
-
 	dayjs: {
 		locales: ["en", "fr"],
 		plugins: ["relativeTime", "utc", "timezone"],
 		defaultLocale: "fr",
 		defaultTimezone: "Europe/Paris",
 	},
-
 	pinia: {
 		autoImports: ["defineStore", "acceptHMRUpdate"],
 	},
