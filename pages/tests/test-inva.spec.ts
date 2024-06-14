9a9ffd63f6af4ecbf730cd602ab199c77f9ca985
David FYON <davidfyon@gmail.com>
2024-06-14 11:23:17 +0200

    rename tests

--- pages/tests/test-inva.spec.ts
+++ pages/tests/test-inva.spec.ts
@@ -4,7 +4,7 @@ import { mountSuspended } from '@nuxt/test-utils/runtime';
 import { describe, it, expect } from 'vitest';
 import { vuetify } from '@/tests/unit/setup';
 
-describe('home page should render', () => {
+describe('test inva page should render', () => {
 	it('should render', async () => {
 		const wrapper = await mountSuspended(TestInva,{
 			global: {
--- pages/tests/tokens.spec.ts
+++ pages/tests/tokens.spec.ts
@@ -4,7 +4,7 @@ import { mountSuspended } from '@nuxt/test-utils/runtime';
 import { describe, it, expect } from 'vitest';
 import { vuetify } from '@/tests/unit/setup';
 
-describe('home page should render', () => {
+describe('tokens page should render', () => {
 	it('should render', async () => {
 		const wrapper = await mountSuspended(Tokens,{
 			global: {
