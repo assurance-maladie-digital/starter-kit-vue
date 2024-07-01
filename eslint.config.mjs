48a03a576f84bb57fd9b140467afed7e7c63854d
David FYON <davidfyon@gmail.com>
2024-07-01 15:17:15 +0200

    add rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt({
 	rules: {
 		'vue/html-self-closing': 'off',
+		'vue/multi-word-component-names': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
 	},
 })
