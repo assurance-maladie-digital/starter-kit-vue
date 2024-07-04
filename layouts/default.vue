d41a1af64901b19b1e2d5a4146aae238253b14c7
virginieVachet <vachetvirginie@gmail.com>
2024-07-04 14:37:58 +0200

    test all components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,6 @@
 <template>
 	<VApp>
-		<HeaderBar
-			service-title="Titre du service"
-			service-sub-title="Description du service"
-			:navigation-items="navigationItems"
-			home-link="/home"
-		/>
+		<AppHeader/>
 		<PageContainer>
 			<slot />
 		</PageContainer>
@@ -24,9 +19,11 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { PageContainer, HeaderBar, FooterBar } from '@cnamts/synapse-bridge'
+import AppHeader from "~/pages/commons/AppHeader.vue";
 
 export default defineComponent({
 	components: {
+		AppHeader,
 		PageContainer,
 		HeaderBar,
 		FooterBar,
@@ -34,22 +31,7 @@ export default defineComponent({
 	data() {
 		return {
 			navigationItems: [
-				{
-					label: 'Accueil',
-					to: '/home',
-				},
-				{
-					label: 'Tokens',
-					to: '/tokens',
-				},
-				{
-					label: 'Maintenance',
-					to: '/maintenance',
-				},
-				{
-					label: 'Erreur',
-					to: '/not-found',
-				},
+
 			],
 		}
 	},
---
+++ pages/maintenance/index.vue
@@ -0,0 +1,88 @@
+<template>
+	<PageContainer>
+		<BackBtn to="/"/>
+		<MaintenancePage />
+	</PageContainer>
+	<FooterBar
+		hide-a11y-link
+		hide-cgu-link
+		hide-cookies-link
+		hide-legal-notice-link
+		hide-sitemap-link
+	>
+		<p class="text--secondary mb-0">Contenu supplémentaire.</p>
+	</FooterBar>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {
+	PageContainer,
+	HeaderBar,
+	FooterBar,
+	DataList,
+	DataListGroup,
+	UserMenuBtn,
+	DialogBox,
+	FooterWrapper,
+	SubHeader,
+	BackBtn,
+	FranceConnectBtn,
+	CopyBtn,
+	Logo,
+	HeaderLoading,
+	BackToTopBtn,
+	CookieBanner,
+	CookiesPage,
+	LogoBrandSection,
+	ChipList,
+	NotificationBar,
+	ErrorPage,
+	MaintenancePage,
+	NotFoundPage,
+	PasswordField,
+	DatePicker,
+	PeriodField
+} from "@cnamts/synapse-bridge";
+import AppHeader from "@/pages/commons/AppHeader.vue";
+
+export default defineComponent({
+	components: {
+		AppHeader,
+		PageContainer,
+		HeaderBar,
+		FooterBar,
+		UserMenuBtn,
+		DialogBox,
+		DataList,
+		DataListGroup,
+		FooterWrapper,
+		SubHeader,
+		BackBtn,
+		FranceConnectBtn,
+		CopyBtn,
+		Logo,
+		HeaderLoading,
+		BackToTopBtn,
+		CookieBanner,
+		CookiesPage,
+		LogoBrandSection,
+		ChipList,
+		NotificationBar,
+		ErrorPage,
+		MaintenancePage,
+		NotFoundPage,
+		PasswordField,
+		DatePicker,
+		PeriodField
+	},
+	data() {
+		return {
+		}
+	},
+	computed: {
+	},
+	methods: {
+	}
+});
+</script>
