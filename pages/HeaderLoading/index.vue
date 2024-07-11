f01d44c9f2ed21302b449399842d2151e842545c
David FYON <davidfyon@gmail.com>
2024-07-11 14:23:21 +0200

    add HeaderLoading

---
+++ pages/HeaderLoading/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<HeaderLoading />
+	<br />
+	<HeaderLoading width="300px" />
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {
+	HeaderLoading,
+} from "@cnamts/synapse-bridge";
+export default defineComponent({
+	components: {
+		HeaderLoading,
+	},
+});
+</script>
