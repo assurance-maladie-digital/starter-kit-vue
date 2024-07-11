ab73e0a6aad8126f81e66b226e803de3cde0ab74
David FYON <davidfyon@gmail.com>
2024-07-11 15:20:01 +0200

    add ChipList

---
+++ pages/chipList/index.vue
@@ -0,0 +1,30 @@
+<template>
+	<ChipList :items="items" />
+
+	<ChipList
+		:items="items"
+		:overflow-limit="2"
+	/>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {
+	ChipList,
+} from "@cnamts/synapse-bridge";
+
+export default defineComponent({
+	components: {
+		ChipList,
+	},
+	data() {
+		return {
+			items: [
+				{ text: 'Item 1', value: 'item1' },
+				{ text: 'Item 2', value: 'item2' },
+				{ text: 'Item 3', value: 'item3' },
+			],
+		}
+	}
+});
+</script>
