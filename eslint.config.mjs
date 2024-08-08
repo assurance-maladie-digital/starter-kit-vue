da976b1b2990a4872e343c96e38dc4edeb5edda1
David FYON <davidfyon@gmail.com>
2024-08-08 10:47:32 +0200

    fix notch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt({
+	files: ['**/*.{js,ts,vue}'],
+	ignores: ['.config/*', 'cypress/**/*', 'cypress.config.js'],
 	rules: {
 		'vue/html-self-closing': 'off',
 		'vue/multi-word-component-names': 'off',
