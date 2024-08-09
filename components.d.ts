7d40c5f0c65ece50ae3f9c20093301fda5f67b2a
David FYON <davidfyon@gmail.com>
2024-08-09 09:37:39 +0200

    update DS

--- components.d.ts
+++ components.d.ts
@@ -6,9 +6,9 @@
 export {}
 
 declare module 'vue' {
-	export interface GlobalComponents {
-		HelloWorld: (typeof import('./components/HelloWorld/HelloWorld.vue'))['default']
-		RouterLink: (typeof import('vue-router'))['RouterLink']
-		RouterView: (typeof import('vue-router'))['RouterView']
-	}
+  export interface GlobalComponents {
+    HelloWorld: typeof import('./components/HelloWorld/HelloWorld.vue')['default']
+    RouterLink: typeof import('vue-router')['RouterLink']
+    RouterView: typeof import('vue-router')['RouterView']
+  }
 }
